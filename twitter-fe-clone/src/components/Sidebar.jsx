d704c11d98bc71d610f96fa05ce99d444c72533b
lil-dev16 <olusegunkd2017@gmail.com>
2023-05-01 00:22:48 +0100

    setup

---
+++ twitter-fe-clone/src/components/Sidebar.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Sidebar = () => {
+    return(
+       <div className="">
+          <div>hellooooooo</div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+       </div>
+    )
+}
+
+export default Sidebar
