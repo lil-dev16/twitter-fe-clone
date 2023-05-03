e8167b215c3b157c4f2a8456339f89048b6fb814
lil-dev16 <olusegunkd2017@gmail.com>
2023-05-03 18:57:44 +0100

    sidebar work

--- twitter-fe-clone/src/components/MainContent.jsx
+++ twitter-fe-clone/src/components/MainContent.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 const MainContent = () => {
   return (
     <div className=''>
-      <h1>Home</h1>
+      <h1 className='text-3xl font-bold'>Home</h1>
       <div className='flex justify-between'>
-        <h3>For you</h3>
-        <h3>Following</h3>
+        <h3 className='tet-2xl font-bold'>For you</h3>
+        <h3 className='tet-2xl font-bold'>Following</h3>
       </div>
       <div className='new-tweet-sec'>
         
